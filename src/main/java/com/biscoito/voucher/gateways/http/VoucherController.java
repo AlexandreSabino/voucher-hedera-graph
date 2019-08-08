39f5e9a479f4860d1ca8966f18abd1079ed695ac
André Mafra <andresmafra@gmail.com>
2019-08-08 14:11:41 -0300

    controller

--- src/main/java/com/biscoito/voucher/gateways/http/VoucherController.java
+++ src/main/java/com/biscoito/voucher/gateways/http/VoucherController.java
@@ -1,12 +1,26 @@
 package com.biscoito.voucher.gateways.http;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/voucher")
 public class VoucherController {
 
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void createVoucher() {
 
+    }
 
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public void debitVoucher() {
+
+    }
 }
