1db6ef828295ef85aa52917e027cd84cb2bcb196
André Mafra <andresmafra@gmail.com>
2019-08-08 14:52:13 -0300

    docker

--- src/main/java/com/biscoito/voucher/usecases/TransferToNetshoes.java
+++ src/main/java/com/biscoito/voucher/usecases/TransferToNetshoes.java
@@ -1,4 +1,17 @@
 package com.biscoito.voucher.usecases;
 
+import com.biscoito.voucher.domains.Voucher;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransferToNetshoes {
+
+
+
+//    public void transfer(final Voucher voucher) {
+//        var client = ExampleHelper.createHederaClient();
+//        client.transferCryptoTo(AccountId.fromString("0.0.3"), 10_000);
+//    }
+
 }
