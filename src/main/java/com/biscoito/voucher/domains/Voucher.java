56c97b45921ddeaf410c91a261bba44f2dbc1d2e
AlexandreSabino <alexandre.sabino.avsd@gmail.com>
2019-08-08 14:09:22 -0300

    adicionando o lombok

--- src/main/java/com/biscoito/voucher/domains/Voucher.java
+++ src/main/java/com/biscoito/voucher/domains/Voucher.java
@@ -1,7 +1,12 @@
 package com.biscoito.voucher.domains;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Collection;
 
+@Getter
+@Setter
 public class Voucher {
 
     private String id;
