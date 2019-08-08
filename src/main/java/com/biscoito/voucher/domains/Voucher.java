d7e17b9f26e34bc8123f3d50068a0e2fdec8a4c4
André Mafra <andresmafra@gmail.com>
2019-08-08 14:05:53 -0300

    voucher obj

--- src/main/java/com/biscoito/voucher/domains/Voucher.java
+++ src/main/java/com/biscoito/voucher/domains/Voucher.java
@@ -1,4 +1,14 @@
 package com.biscoito.voucher.domains;
 
+import java.util.Collection;
+
 public class Voucher {
+
+    private String id;
+
+    private String clientId;
+
+    private Long amount;
+
+    private Collection<String> events;
 }
