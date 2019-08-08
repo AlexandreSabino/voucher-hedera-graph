56a52960a42905b7109b381f0e45829e023984b8
Eder Magalhães <edermag@gmail.com>
2019-08-08 15:40:54 -0300

    part II transferToCustomer

---
+++ src/main/java/com/biscoito/voucher/domains/VoucherEvent.java
@@ -0,0 +1,20 @@
+package com.biscoito.voucher.domains;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class VoucherEvent {
+
+  private String id;
+  private byte[] transactionHash;
+  private String description;
+  private LocalDateTime when;
+  private long amount;
+  private long transationFee;
+
+}
