d39850a3614fc197ecb94bd40fcf99c8296346e8
AlexandreSabino <alexandre.sabino.avsd@gmail.com>
2019-08-08 15:11:48 -0300

    FindOrCreateCustomerAccount usecase

---
+++ src/main/java/com/biscoito/voucher/gateways/HederaHelper.java
@@ -0,0 +1,49 @@
+package com.biscoito.voucher.gateways;
+
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.account.AccountId;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Component
+public class HederaHelper {
+
+    @Value("${hedera.NODE_ADDRESS}")
+    private String nodeAddress;
+
+    @Value("${hedera.NODE_ID}")
+    private String nodeId;
+
+    @Value("${hedera.OPERATOR_ID}")
+    private String operatorId;
+
+    @Value("${hedera.OPERATOR_KEY}")
+    private String operatorKey;
+
+    public AccountId getNodeId() {
+        return AccountId.fromString(nodeId);
+    }
+
+    public AccountId getOperatorId() {
+        return AccountId.fromString(operatorId);
+    }
+
+    public Ed25519PrivateKey getOperatorKey() {
+        return Ed25519PrivateKey.fromString(operatorKey);
+    }
+
+    public Client createHederaClient() {
+        // To connect to a network with more nodes, add additional entries to the network map
+        var client = new Client(Map.of(getNodeId(), nodeAddress));
+
+        // Defaults the operator account ID and key such that all generated transactions will be paid for
+        // by this account and be signed by this key
+        client.setOperator(getOperatorId(), getOperatorKey());
+
+        return client;
+    }
+}
--- src/main/java/com/biscoito/voucher/usecases/FindOrCreateCustomerAccount.java
+++ src/main/java/com/biscoito/voucher/usecases/FindOrCreateCustomerAccount.java
@@ -1,4 +1,41 @@
 package com.biscoito.voucher.usecases;
 
+import com.biscoito.voucher.domains.Customer;
+import com.biscoito.voucher.gateways.CustomerGateway;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.HederaException;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class FindOrCreateCustomerAccount {
+
+    private final CustomerGateway customerGateway;
+
+    private final Client client;
+
+    public Customer execute(final String customerIdentifier) {
+        final Optional<Customer> customerOptional = customerGateway.findOne(customerIdentifier);
+        return customerOptional.orElseGet(() -> {
+            try {
+                var newKey = Ed25519PrivateKey.generate();
+                var newAccountId = client.createAccount(newKey.getPublicKey(), 0);
+
+                return Customer.builder()
+                        .accountId(newAccountId.toString())
+                        .customerIdentifier(customerIdentifier)
+                        .build();
+
+            } catch (HederaException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+    }
 }
