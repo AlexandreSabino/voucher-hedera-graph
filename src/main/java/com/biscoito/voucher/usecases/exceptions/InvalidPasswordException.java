7574978bef4eddf320465f1d9990d278792dd2f7
André Mafra <andresmafra@gmail.com>
2019-08-09 09:54:36 -0300

    update app

--- src/main/java/com/biscoito/voucher/usecases/exceptions/InvalidPasswordException.java
+++ src/main/java/com/biscoito/voucher/usecases/exceptions/InvalidPasswordException.java
@@ -1,5 +1,9 @@
 package com.biscoito.voucher.usecases.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class InvalidPasswordException extends RuntimeException {
 
     public InvalidPasswordException(String message) {
